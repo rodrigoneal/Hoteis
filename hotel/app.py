ae4b110711827764970316551a28d5889c20faa6
rodrigoneal <rodrigho2006@gmail.com>
2021-01-26 18:44:23 -0300

    Passando arquivos no linter

--- hotel/app.py
+++ hotel/app.py
@@ -7,8 +7,3 @@ def create_app():
     configuration.init_app(app)
     configuration.load_extensions(app)
     return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
